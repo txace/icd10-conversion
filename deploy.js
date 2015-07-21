1fb4fd13dfe5fbe3312aec7eb8dd518ebe1ae498
Tim Markello <timmarkello@gmail.com>
2015-07-21 09:55:37 -0500

    adding project deployment scritps

---
+++ deploy.js
@@ -0,0 +1,37 @@
+var util = require('mis-util');
+var config = require('./config.ignore');
+
+var options = {
+   sysname: '/c1/FRSH',
+   connect: {
+      host: 'gccmhc',
+      user: 'tim',
+      password: config.user
+   },
+   cron: {
+      user: 'datamgr',
+      pass: config.cron
+   },
+   view_path: {
+      local: './view/',
+      remote: '/CUST/forms/'
+   },
+   parm_path: {
+      local: './build/'
+   },
+   usc_path: {
+      local: './'
+   }
+};
+
+var mis = util(options);
+
+console.log('deploying to: ' + options.sysname);
+
+mis.parm.fromflatfile('./icdmap.ignore')
+.then(function(parm) { 
+   console.log('writing parm')
+   return mis.parm.tofile('./build/ICDMAP.parm', parm)
+})
+.then(mis.deploy.parm)
+.then(mis.script.runonce.bind(mis, './ConICDSQL.usc'));
